9c45d7dff99020c2c1efd28a094d919008d6f4ee
Johnny Bhojwani <johnnyvb@gmail.com>
2022-10-17 22:27:35 -0500

    initial commit

---
+++ keys.py
@@ -0,0 +1,11 @@
+consumer_key = 'Mpq99uFFzn8fyj9lPVQZb7rEs'
+consumer_secret = 'DpsbC4t76HVGXmnW8Tlh0ypYTOSS9ValCWcKAtNYQ4g5vXGLGv'
+access_token = '2190430531-HX94SgcxHIJtNKDnEcKTGihkOueACS8ZAQUA1yh'
+access_token_secret = 'cPya7vG3lw3W3LTPG87m9VYro9T42mblkyXM8Y4buF6WW'
+
+mapquest_key = 'YourAPIKey'
+
+
+api_key =  'ERLlmRmvt26YebhbmafevvUlB'
+api_key_secret = '0rL3N8CIItQF8MBCu5zruJYqGE7Ki9NYPTEawkML9BlS6Y8waB'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMKjCgEAAAAAEWqvx6mE3RoX4%2FMmX9Pmk8fWwuA%3DEApIMyOqcDhcf2iwBoL20glObsuADw0TKoEu43jlWqGlpKagIU'
